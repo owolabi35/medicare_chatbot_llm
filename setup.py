cc83aa398340be38aac6e529fbd4d122b2272f47
owolabi35 <tjowo14@gmail.com>
2025-03-19 12:07:33 -0700

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Tajudeen Sanni',
+    author_email= 'tjowo14@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
